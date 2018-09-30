eadbf6459a1846297e0795600e13e2415f2edbd2
feiying <924568531@qq.com>
2018-09-30 10:54:13 +0800

    IDEA关联GIT，code-01

---
+++ Module01/src/com/ecen/test.java
@@ -0,0 +1,15 @@
+package com.ecen;
+
+/**
+ * @author Feiying
+ * 2018/9/30 10:44
+ */
+public class test {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+
+}
